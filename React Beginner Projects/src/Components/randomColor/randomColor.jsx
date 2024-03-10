7e823da1ac6e1bba456bb892b9165e5387394e08
Ohbra <brianwaweru58@gmail.com>
2024-03-10 20:34:08 +0100

    background Color implementation 3

--- React Beginner Projects/src/Components/randomColor/randomColor.jsx
+++ React Beginner Projects/src/Components/randomColor/randomColor.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function RandomColor(){
-    const[colorType, setColorType] = useState(null);
+export default function RandomColor() {
+  const [colorType, setColorType] = useState(null);
+  const color = "pink";
 
-    console.log("colorType -> "+colorType)
-    
-    function handleRGB(){
-        console.log("handleRGB -> "+colorType)
-    }
+  console.log("colorType -> " + colorType);
 
-    function handleHEX(){
-        console.log("handleHEX -> "+colorType)
-    }
+  function handleRGB() {
+    console.log("handleRGB -> " + colorType);
+  }
 
-    return(
-        <div>
-            <button onClick={()=>setColorType("RGB")}>Change RGB</button>
-            <button onClick={()=>setColorType("HEX")}>Change HEX</button>
-            <button onClick={colorType == "RGB" ? handleRGB : handleHEX}>Generate Random Color</button>
+  function handleHEX() {
+    console.log("handleHEX -> " + colorType);
+  }
 
-        </div>
-    )
+  return (
+      <div   style={{ 
+        width:"100vw",
+        height:"100vh",
+        backgroundColor: color}}
+        >
+        <button onClick={() => setColorType("RGB")}>Change RGB</button>
+        <button onClick={() => setColorType("HEX")}>Change HEX</button>
+        <button onClick={colorType == "RGB" ? handleRGB : handleHEX}>
+          Generate Random Color
+        </button>
+      </div>
+
+  );
 }
